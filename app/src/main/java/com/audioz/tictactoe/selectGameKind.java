6c686a3bee9a89e9c902659add8460d6aef7d5d2
audioz <73185455+ignaudioz@users.noreply.github.com>
2023-06-12 11:34:02 +0300

    fixed spaghetti code. Todo: Fix firebase value pointing to a null object.

---
+++ app/src/main/java/com/audioz/tictactoe/selectGameKind.java
@@ -0,0 +1,45 @@
+package com.audioz.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class selectGameKind extends AppCompatActivity {
+
+    //SharedPreference
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    //Buttons
+    ImageButton btnleave;
+    Button localbtn,multibtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //Buttons
+        btnleave = findViewById(R.id.btnleave);
+
+//        botbtn = findViewById(R.id.botbtn);
+        localbtn = findViewById(R.id.localbtn);
+        multibtn = findViewById(R.id.multibtn);
+
+        //onClicks
+        btnleave.setOnClickListener(view -> finish());
+
+        localbtn.setOnClickListener(view -> {
+            Intent i = new Intent(this, localgame.class);
+            startActivity(i);
+        });
+        multibtn.setOnClickListener(view -> {
+            Intent i = new Intent(this, login.class);
+            startActivity(i);
+        });
+
+    }
+}
